256c39cedc93e4789c6e86193f5455c4f0cbc781
Fadla Ichsan <fadla.ichsan@weekendinc.com>
2026-05-23 17:33:12 +0700

    add faq section

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,1 +1,9 @@
-console.log('accordion')
+const accordionItems = document.querySelectorAll('.faq__item');
+
+accordionItems.forEach((item) => {
+    const accordion = item.querySelector('.faq__accordion');
+
+    accordion.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
