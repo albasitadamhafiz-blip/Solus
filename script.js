1e5bcce9d6c91de4d0974818130bfe28fbb706d4
Fadla Ichsan <fadla.ichsan@weekendinc.com>
2026-05-27 16:39:41 +0700

    add testimonials section

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 import './scripts/accordion.js'
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('.splide', {
+    perPage: 1,
+    gap: '40px',
+    pagination: false,
+    arrows: false,
+    fixedWidth: '39.3rem',
+    perMove: 1,
+  });
+
+  splide.mount();
+
+  const prevButton = document.querySelector('.testimonials__button--prev');
+  const nextButton = document.querySelector('.testimonials__button--next');
+
+  prevButton.addEventListener('click', () => {
+    splide.go('<');
+  });
+
+  nextButton.addEventListener('click', () => {
+    splide.go('>');
+  });
+});
